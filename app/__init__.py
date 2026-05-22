f1ac05838a6c0ebbe17bf0b329e534e8792a022d
Kobydon <94402053+Kobydon@users.noreply.github.com>
2026-05-22 20:16:03 +0000

    update

--- app/__init__.py
+++ app/__init__.py
@@ -14,15 +14,16 @@ def create_app():
 
     # ✅ Configure CORS properly
     CORS(app, 
-         resources={r"/*": {
-             "origins": ["http://localhost:4200", "http://127.0.0.1:4200", "http://localhost:5000","https://tabital.onrender.com",
-                         "https://app.tabitalpay.com"],
-             "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-             "allow_headers": ["Content-Type", "Authorization", "X-Requested-With"],
-             "expose_headers": ["Content-Type", "Authorization"],
-             "supports_credentials": True,
-             "max_age": 3600
-         }})
+        #  resources={r"/*": {
+        #      "origins": ["http://localhost:4200", "http://127.0.0.1:4200", "http://localhost:5000","https://tabital.onrender.com",
+        #                  "https://app.tabitalpay.com"],
+        #      "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+        #      "allow_headers": ["Content-Type", "Authorization", "X-Requested-With"],
+        #      "expose_headers": ["Content-Type", "Authorization"],
+        #      "supports_credentials": True,
+        #      "max_age": 3600
+        #  }}
+        )
 
     # Initialize extensions
     db.init_app(app)
